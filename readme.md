f8d3f8b646e078eefd71270299b9e0e4b7377b90
TNTSuperMan <tntsuperga@gmail.com>
2024-08-21 20:54:55 +0900

    MD

--- readme.md
+++ readme.md
@@ -3,7 +3,4 @@
 ## なんこれ
 VTuberアプリを作ってるつもり
 ## 進行状況
-HTTP通信系は.NET Frameworkのv2dsynthに丸投げ  
-口は動く、瞬きはする、めちゃいいかんじ  
-v2dsynthのライセンスが心配  
-7月分のv0.6が出せるか心配で(泣く)
+AniLuaというLuaスクリプト作ってるけど謎バグでFrameに空のPrintしないとエラー吐く
--- v2d/anilua.cpp
+++ v2d/anilua.cpp
@@ -34,6 +34,10 @@ void PushSpriteTable(Sprite sprite, const char* name) {
 	lua_pushnumber(L, sprite.Deg);
 	lua_settable(L, -3);
 
+	lua_pushstring(L, "Image");
+	lua_pushinteger(L, sprite.Image);
+	lua_settable(L, -3);
+
 	lua_pushstring(L, "Size");
 	lua_pushnumber(L, sprite.Size);
 	lua_settable(L, -3);
@@ -69,6 +73,10 @@ void ReadSpriteTable(Sprite* sprite, const char* name) {
 	sprite->Deg = luaL_checknumber(L, -1);
 	lua_pop(L, 1);
 
+	lua_getfield(L, -1, "Image");
+	sprite->Image = luaL_checkinteger(L, -1);
+	lua_pop(L, 1);
+
 	lua_getfield(L, -1, "Size");
 	sprite->Size = luaL_checknumber(L, -1);
 	lua_pop(L, 1);
@@ -80,6 +88,7 @@ void ReadSpriteTable(Sprite* sprite, const char* name) {
 	sprite->Pos = ReadVectorTable("Pos");
 	sprite->Cent = ReadVectorTable("Cent");
 	sprite->Stren = ReadVectorTable("Stren");
+	lua_pop(L,2);
 }
 
 #pragma endregion
@@ -87,6 +96,7 @@ void ReadSpriteTable(Sprite* sprite, const char* name) {
 
 void GetAniluaState(Sprite *head, Sprite* body, Sprite* eye1, Sprite* eye2, Sprite* mouth) {
 	if (is_loading) return;
+	lua_settop(L,0);
 	lua_newtable(L);
 	PushSpriteTable(*head, "Head");
 	PushSpriteTable(*body, "Body");
@@ -97,35 +107,56 @@ void GetAniluaState(Sprite *head, Sprite* body, Sprite* eye1, Sprite* eye2, Spri
 
 	lua_getglobal(L, "Frame"); //Func
 	lua_pushnumber(L, GetNowCount() - startms);
-	if (lua_pcall(L, 1, 1, 0) != 0) {
-		printfDx("AniLuaでエラーが発生しました☆:%s\n", lua_tostring(L, -1));
-		lua_close(L);
-		is_loading = true;
-		return;
-	} else {
-		bool is_next = lua_toboolean(L, -1);
-		ReadSpriteTable(head, "Head");
-		ReadSpriteTable(body, "Body");
-		ReadSpriteTable(eye1, "Eye1");
-		ReadSpriteTable(eye2, "Eye2");
-		ReadSpriteTable(mouth, "Mouth");
-		if (!is_next) {
+	try {
+		if (lua_pcall(L, 1, 1, 0) != 0) {
+			printfDx("AniLuaでエラーが発生しました☆:%s\n", lua_tostring(L, -1));
 			lua_close(L);
 			is_loading = true;
 			return;
 		}
+		else {
+			bool is_next = lua_toboolean(L, -1);
+			ReadSpriteTable(head, "Head");
+			ReadSpriteTable(body, "Body");
+			ReadSpriteTable(eye1, "Eye1");
+			ReadSpriteTable(eye2, "Eye2");
+			ReadSpriteTable(mouth, "Mouth");
+			if (!is_next) {
+				lua_close(L);
+				is_loading = true;
+				return;
+			}
+		}
+	}
+	catch(...){
+		printfDx("あ");
+		is_loading = true;
+		lua_close(L);
+		return;
 	}
 }
 
 int Lprint(lua_State* Le) {
 	const char* str = luaL_checkstring(Le, 1);
 	printfDx(str);
-	return 1;
+	return 0;
 }
 
 int Lclear(lua_State* Le) {
 	clsDx();
+	return 0;
+}
+
+int LloadGraph(lua_State* Le) {
+	int id = LoadGraph(luaL_checkstring(Le, 1));
+	lua_pushinteger(Le, id);
 	return 1;
+}
+
+int LdeleteGraph(lua_State* Le) {
+	int id = luaL_checkinteger(L, 1);
+	DeleteGraph(id);
+	return 0;
 }
 
 void AniLuaMsg(WPARAM wp, LPARAM lp) {
@@ -148,6 +179,12 @@ void AniLuaMsg(WPARAM wp, LPARAM lp) {
 
 		lua_pushcfunction(L, Lclear);
 		lua_setglobal(L, "cls");
+
+		lua_pushcfunction(L, LloadGraph);
+		lua_setglobal(L, "loadGraph");
+
+		lua_pushcfunction(L, LdeleteGraph);
+		lua_setglobal(L, "deleteGraph");
 
 		if (lua_pcall(L, 0, 0, 0) != 0) {
 			printfDx("AniLuaでエラーが発生しました:\n%s\n", lua_tostring(L, -1));
